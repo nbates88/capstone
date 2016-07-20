4949376ba6f21b9dcb53281300ac579b7275f9cf
nbates88 <nbates88@gmail.com>
2016-07-20 21:40:37 +0200

    Update index.js

--- server/app/routes/applications/index.js
+++ server/app/routes/applications/index.js
@@ -23,7 +23,7 @@ var transporter = nodemailer.createTransport(smtpTransport({
     service: "Gmail",
     auth: {
         user: "grasshopperreporter@gmail.com",
-        pass: "vEzu8_cHAbraxaz",
+        pass: "",
     }
 }));
 
